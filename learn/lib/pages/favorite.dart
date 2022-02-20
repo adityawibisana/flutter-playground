e3a9f0a98545e44d462fe4b83b79a1e9b188e20e
Aditya Wibisana <aditya.wibisana@gmail.com>
2022-02-20 16:34:48 +0700

    Add direct runner to Favorite screen

--- learn/lib/pages/favorite.dart
+++ learn/lib/pages/favorite.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const FavoriteDirectRunner());
+}
+
+class FavoriteDirectRunner extends StatelessWidget {
+  const FavoriteDirectRunner({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Favorite(),
+    );
+  }
+}
+
 class Favorite extends StatelessWidget {
   const Favorite({Key? key}) : super(key: key);
 
