463dcd3cb95a638d082daaf842f8adb42b2f0f1e
Aditya Wibisana <aditya.wibisana@gmail.com>
2022-02-22 15:27:41 +0700

    Added flutter_bloc add unit test for counter cubit

---
+++ learn/test/cubit_test.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: avoid_print
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit(int initialState) : super(initialState);
+
+  void increment() {
+    emit(state + 1);
+  }
+
+  void decrement() => emit(state - 1);
+}
+
+void main() async {
+  test('Cubit learn test', () async {
+    var counter = CounterCubit(0);
+    counter.increment();
+    expect(1, counter.state, reason: "Because 0 + 1 is 1");
+    counter.decrement();
+    expect(2, counter.state);
+  });
+}
