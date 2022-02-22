3825b92e14c35cb91f9ad53cfc12c39c1c2fc6a5
Aditya Wibisana <aditya.wibisana@gmail.com>
2022-02-22 16:49:35 +0700

    Add bloc unit test

---
+++ learn/test/bloc_test.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: avoid_print
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc(int initialState) : super(initialState) {
+    on<CounterEvent>((event, emit) {
+      switch (event) {
+        case CounterEvent.increment:
+          emit(state + 1);
+          break;
+        case CounterEvent.decrement:
+          emit(state - 1);
+          break;
+      }
+    });
+  }
+}
+
+void main() async {
+  test('Bloc learn test increment', () async {
+    var counterBloc = CounterBloc(0);
+
+    var subscription = counterBloc.stream.listen((event) {
+      expect(event, 1);
+    });
+    counterBloc.add(CounterEvent.increment);
+
+    await Future.delayed(Duration.zero);
+    await counterBloc.close();
+  });
+}
